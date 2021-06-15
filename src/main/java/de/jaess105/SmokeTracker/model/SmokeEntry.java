d5a7092e05d4c3a5098cfe543a12a9aae3edac38
Jannik Esser <jaess105@uni-duesseldorf.de>
2021-06-15 12:22:49 +0200

    fuck time zones :upside_down_face:

--- src/main/java/de/jaess105/SmokeTracker/model/SmokeEntry.java
+++ src/main/java/de/jaess105/SmokeTracker/model/SmokeEntry.java
@@ -1,6 +1,7 @@
 package de.jaess105.SmokeTracker.model;
 
 import java.time.LocalDateTime;
+import java.time.ZoneId;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -23,11 +24,11 @@ public class SmokeEntry {
     this.zeitstempel = zeitstempel;
   }
 
-  public static SmokeEntry makeEntryNow(){
-    return new SmokeEntry(LocalDateTime.now());
+  public static SmokeEntry makeEntryNow() {
+    return new SmokeEntry(LocalDateTime.now().atZone(ZoneId.of("Europe/Berlin")).toLocalDateTime());
   }
 
-  public String toCSVLine(){
-    return String.format("%s,%s",id,zeitstempel);
+  public String toCSVLine() {
+    return String.format("%s,%s", id, zeitstempel);
   }
 }
