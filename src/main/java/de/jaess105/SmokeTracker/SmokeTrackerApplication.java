edd34d69e58da0de001f2a3e2b93ba23bcf07337
Jannik Esser <jaess105@uni-duesseldorf.de>
2021-06-15 09:25:54 +0200

    first commit

---
+++ src/main/java/de/jaess105/SmokeTracker/SmokeTrackerApplication.java
@@ -0,0 +1,13 @@
+package de.jaess105.SmokeTracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmokeTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmokeTrackerApplication.class, args);
+	}
+
+}
